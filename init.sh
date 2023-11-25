735d8269a4a9573afd9c6433a71f34502ccfccd3
bear-is-asleep <bcarlson1@ufl.edu>
2023-11-24 18:11:40 -0600

    cpu stable branch

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
-python3.9 -m venv env
+if command -v python3 &>/dev/null; then
+  python3 -m venv env
+else
+  echo "Python 3 is not installed. Please install Python 3 and try again."
+  exit 1
+fi
 source env/bin/activate
 which python
 pip install --upgrade pip
